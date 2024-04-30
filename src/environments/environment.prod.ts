cf457244535759cc4d5b48bd246c4e3d2304848e
Alok Singh Narvariya <47643550+alok144@users.noreply.github.com>
2024-05-01 02:03:40 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // apiEndPoint: 'http://localhost:3000/'
-  apiEndPoint: 'https://wallet-api-1g8l.onrender.com/'
+  apiEndPoint: 'https://wallet-api-1g8l.onrender.com/api/v1/'
 };
